a41f598ed7a3c8e5b4196b7768229c1abfaee457
Artur Istvan Karoly <artur.karoly@irob.uni-obuda.hu>
2023-04-27 16:04:51 +0200

    Add output saving

--- render_frames.py
+++ render_frames.py
@@ -1,5 +1,6 @@
 import bpy
 import sys
+import os
 
 def parse_args():
     '''
@@ -12,10 +13,11 @@ def parse_args():
         argv = argv[argv.index("--") + 1:]  # get all args after "--"
 
     class Args():
-        def __init__(self, frame_start=0, frame_num=1, gpu=False):
+        def __init__(self, frame_start=0, frame_num=1, gpu=False, output_dir='./tmp_render_out'):
             self.frame_start = frame_start
             self.frame_num = frame_num
             self.gpu = gpu
+            self.output_dir = output_dir
 
     args = Args()
 
@@ -26,6 +28,8 @@ def parse_args():
             args.frame_num = int(element.split(' ')[-1])
         if '--gpu' in element:
             args.gpu = True
+        if '-o' in element:
+            args.output_dir = element.split(' ')[-1]
    
     return args
 
@@ -38,7 +42,7 @@ def main():
     for i in range(args.frame_num):
         current_frame = start_frame + i
         scene.frame_set(current_frame) # Set frame
-        scene.render.filepath = scene.render.frame_path(frame=current_frame)
+        scene.render.filepath = os.path.join(args.output_dir, os.path.split(scene.render.frame_path(frame=current_frame))[-1])
         bpy.ops.render.render(write_still=True) # Render photorealistic image
 
 if __name__=='__main__':
