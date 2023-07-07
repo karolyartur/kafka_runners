c783b2e41307bce6908b92e3e75145e1fbf3cb06
Artur Istvan Karoly <artur.karoly@irob.uni-obuda.hu>
2023-07-07 11:49:02 +0200

    Add service info

--- kafka_runner.py
+++ kafka_runner.py
@@ -1,3 +1,4 @@
+import os
 import io
 import socket
 import json
@@ -8,6 +9,9 @@ import logging
 import traceback
 from kafka import KafkaConsumer, KafkaProducer
 from kafka.errors import KafkaConfigurationError, KafkaTimeoutError
+from jsonschema import validate, RefResolver
+from jsonschema.exceptions import ValidationError
+from json.decoder import JSONDecodeError
 
 parser = argparse.ArgumentParser(description="Kafka Runner")
 parser.add_argument("in_topic_name", type=str,help="in_topic name")
@@ -41,7 +45,7 @@ class KafkaRunner():
      - error_topic_name (str): Name of the Kafka topic to send error logs to (default is 'error.log')
      - loglevel (logging.DEBUG/WARN/...): Logging level (default is logging.WARN meaning warnings and higher level logs will be reported)
     '''
-    def __init__(self, in_topic_name, out_topic_name, bootstrap_servers, consumer_group_id=None, error_topic_name='error.log', loglevel=logging.WARN):
+    def __init__(self, in_topic_name, out_topic_name, bootstrap_servers, consumer_group_id=None, error_topic_name='error.log', service_info_topic_name='service.info', loglevel=logging.WARN):
         '''Constructor for Kafka Runners
         '''
         self.timeout = 30
@@ -50,6 +54,8 @@ class KafkaRunner():
         self.bootstrap_servers = bootstrap_servers
         self.consumer_group_id = consumer_group_id
         self.error_topic_name = error_topic_name
+        self.service_info_topic_name = service_info_topic_name
+        self.service_info = None
 
         # Create logger
         self.logger = logging.getLogger(__name__ + '.' + type(self).__name__ + '@' + socket.gethostname())
@@ -118,6 +124,8 @@ class KafkaRunner():
                             self.logger.info('Sending output: {}'.format(response))
                         print(stdout.strip())
                 print('Idle ...')
+                if self.service_info:
+                    self.producer.send(self.service_info_topic_name, self.service_info)
                 time.sleep(self.timeout)
             except FileNotFoundError as e:
                 self.logger.error('The construced command could not be executed!')
@@ -170,6 +178,37 @@ class KafkaRunner():
             self.error_stream.seek(0)
             self.error_stream.truncate()
         return wrapper
+
+    def construct_and_validate_service_info(self, schema_path, service_name, service_description='', input_topic_schema={}, output_topic_schema={}):
+        '''Helper function for constructing and validating service info JSON messages
+        '''
+        self.service_info_schema = {}
+        # Load Service Info JSON schema
+        try:
+            schema_path = os.path.join(schema_path,'service_info.schema.json')
+            with open(schema_path, 'r') as f:
+                self.service_info_schema = json.loads(f.read())
+        except FileNotFoundError:
+            self.logger.warning('JSON schema for Kafka message could not be found at path: "{}"\nService Info messages will NOT be validated!'.format(schema_path))
+            self.service_info_schema = {}
+        except JSONDecodeError:
+            self.logger.warning('JSON schema for Kafka message at path: "{}" could not be decoded (invalid JSON)\nService Info messages will NOT be validated!'.format(schema_path))
+            self.service_info_schema = {}
+
+        # Construct service info
+        service_info = {}
+        service_info['serviceName'] = service_name
+        service_info['serviceDescription'] = service_description
+        service_info['inputTopics']= [{'topicName': self.in_topic_name, 'schema': input_topic_schema}]
+        service_info['outputTopics'] = [{'topicName': self.out_topic_name, 'schema': output_topic_schema}, {'topicName': self.error_topic_name, 'schema': {}}, {'topicName': self.service_info_topic_name, 'schema': self.service_info_schema}]
+
+        try:
+            resolver = RefResolver(base_uri='file://'+os.path.abspath(schema_path)+'/'+'service_info.schema.json', referrer=None)
+            validate(instance=service_info, schema=self.service_info_schema, resolver=resolver)
+        except ValidationError:
+            self.logger.warning('Service Info "{}" failed JSON schema validation (used schema: {})\nIgnoring message'.format(service_info, os.path.join(schema_path,'service_info.schema.json')))
+        else:
+            return service_info
 
 def main():
     args = parser.parse_args()
