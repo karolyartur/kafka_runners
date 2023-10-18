0696f3577a8586705b7acbcdace4b1415c862ce8
tamas.csibrak@irob.uni-obuda.hu <tamas.csibrak@irob.uni-obuda.hu>
2023-10-18 21:05:59 +0200

    Update kafka-annotate.sh

--- kafka-annotate.sh
+++ kafka-annotate.sh
@@ -2,4 +2,4 @@
 #Xvfb :2 -screen 0 1920x1080x16 &
 #export DISPLAY=:0.0 
 
-python3 kafka_annotate.py $IN_KAFKA_TOPIC $OUT_KAFKA_TOPIC $KAFKA_BROKERS
+python3 kafka_annotate.py $IN_KAFKA_TOPIC $OUT_KAFKA_TOPIC $KAFKA_BROKERS -g $ANNOTATOR_GROUP_ID
