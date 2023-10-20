558ea720d6c8f6924b3c0eb3d45ffe21a734aa3a
tamas.csibrak@irob.uni-obuda.hu <tamas.csibrak@irob.uni-obuda.hu>
2023-10-20 11:09:27 +0000

    Update kafka-annotate.sh remved groupid for testing

--- kafka-annotate.sh
+++ kafka-annotate.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-Xvfb :2 -screen 0 1920x1080x16 &
-export DISPLAY=:0.0 
+#Xvfb :2 -screen 0 1920x1080x16 &
+#export DISPLAY=:0.0 
 
-python3 kafka_annotate.py $IN_KAFKA_TOPIC $OUT_KAFKA_TOPIC $KAFKA_BROKERS -g $ANNOTATOR_GROUP_ID
+python3 kafka_annotate.py $IN_KAFKA_TOPIC $OUT_KAFKA_TOPIC $KAFKA_BROKERS
+
